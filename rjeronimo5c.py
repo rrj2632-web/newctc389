845e9515e94f058d90dfaf14bb5f9dd1797b842a
rrj2632-web <rrj2632@lausd.net>
2026-08-23 21:09:08 +0000

    lab 5c

---
+++ rjeronimo5c.py
@@ -0,0 +1,14 @@
+#Rogelio Jeronimo
+
+#CTC 389 151
+
+#August 23, 2026
+
+#Lab 5 Part 3 Fruits and Vegetables
+
+fruit_veg = ["strawberries","onions","watermelon","cucumber","cabbage"]
+
+for i in fruit_veg:
+    print (i)
+
+
