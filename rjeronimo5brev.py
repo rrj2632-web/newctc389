772aaf4553a4722e597f1b67fa8a62f4c312eec6
rrj2632-web <rrj2632@lausd.net>
2026-09-03 00:59:43 +0000

    Lab 5b Rev

---
+++ rjeronimo5brev.py
@@ -0,0 +1,16 @@
+#Rogelio Jeronimo
+
+#CTC 389 151
+
+#August 23, 2026
+
+#Lab 5 Part 2 With_Loop
+
+numbers = [110,210,310,410,510,610,710,810,910,1010]
+
+print ("With Loop")
+print ()
+for i in numbers:
+    print (i)
+
+
