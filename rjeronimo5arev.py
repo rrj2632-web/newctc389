03ede5c3d423cf1815f56e71aa05936c6b5ee9a0
rrj2632-web <rrj2632@lausd.net>
2026-09-03 00:56:19 +0000

    Lab 5a REV

---
+++ rjeronimo5arev.py
@@ -0,0 +1,23 @@
+#Rogelio Jeronimo
+
+#CTC 389 151
+
+#August 23, 2026
+
+#Lab 5 Part 1 No_Loop
+
+numbers = [110,210,310,410,510,610,710,810,910,1010]
+
+print ("No Loop")
+print()
+print (numbers[0])
+print (numbers[1])
+print (numbers[2])
+print (numbers[3])
+print (numbers[4])
+print (numbers[5])
+print (numbers[6])
+print (numbers[7])
+print (numbers[8])
+print (numbers[9])
+
