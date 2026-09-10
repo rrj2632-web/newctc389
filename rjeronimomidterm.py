b08524b2bcd4a7d9f1780bd008c7c61eda936cfc
rrj2632-web <rrj2632@lausd.net>
2026-09-10 00:34:28 +0000

    Mid Term Program 1

---
+++ rjeronimomidterm.py
@@ -0,0 +1,26 @@
+#Rogelio Jeronimo
+#CTC-389-151
+#Septermber 9, 2026
+
+#Mid-Term Exam Python Program #1
+
+name = input ("Please enter your name: ")
+print()
+
+number = int (input ("Please enter a number: "))
+print()
+
+more = float (input ("How much would you like to add to the number? "))
+print()
+
+result = number + more
+
+print ("Your name is ", name)
+print()
+print ("The number you entered is ", number)
+print()
+print ("The number you want to add to ", number, "is ", more)
+print()
+print (name, " the result is: ", result)
+
+
