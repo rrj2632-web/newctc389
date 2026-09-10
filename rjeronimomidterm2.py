fe091d794553dce395c2fd02ba897d3c8e19462e
rrj2632-web <rrj2632@lausd.net>
2026-09-10 00:35:39 +0000

    Mid Term Program 2

---
+++ rjeronimomidterm2.py
@@ -0,0 +1,20 @@
+#Rogelio Jeronimo
+#CTC-389-151
+#September 9, 2026
+
+#Mid Term Python Program #2
+
+def calculate_area (base, height):
+    area = base * height
+    return area
+
+base = float (input ("Please enter the measurement of the base of the rectangle: "))
+print()
+height = float (input ("Please enter the measurement of the height of the rectanlge: "))
+print()
+
+area = calculate_area (base, height)
+
+print ("The area of th rectangle is ", area)
+
+
